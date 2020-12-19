1feea20a1162190bf4b58e5505eb8d2ff113cd4f
Ekaterina <ekaterinan112@gmail.com>
2020-12-19 23:03:49 +0300

    decorate button component

--- src/common/button/Button.js
+++ src/common/button/Button.js
@@ -2,10 +2,14 @@ import React from 'react'
 import styles from './Button.module.css';
 
 
-
-function Button (props) {
-    return <div className={styles.button}>
-        <button>{props.title}</button>
+function Button(props) {
+    return <div className={styles.buttonBlock}>
+        <button className={styles.button}>
+            <a>
+            {props.title}
+            </a>
+        </button>
     </div>
 }
+
 export default Button;
