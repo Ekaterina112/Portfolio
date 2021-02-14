ab23b385e9bfec7d156b0adfe38adb07aa66d4a2
Ekaterina <ekaterinan112@gmail.com>
2021-02-14 23:45:00 +0300

    effects on buttons, inputs, textarea, links, skills block

--- src/a3_skills/Skills.js
+++ src/a3_skills/Skills.js
@@ -7,19 +7,18 @@ import {faBriefcase, faLaptopCode, faTasks} from "@fortawesome/free-solid-svg-ic
 import {faSketch} from "@fortawesome/free-brands-svg-icons";
 
 
-
 function Skills() {
     return (
         <div className={styles.skillsBlock} id='skills'>
             <div className={`${stylesContainer.container} ${styles.skillsContainer}`}>
                 <Title title={'My Skills'}/>
                 <div className={styles.skills}>
-                    <Skill title ={'DEVELOPMENT'} description={"React, Redux, AXIOS etc"} titleIcon={faLaptopCode}/>
-                    <Skill title ={'FOUNDATION'} description={"JavaScript, TypeScript, HTML5, CSS3"} titleIcon={faBriefcase}/>
-                    <Skill title ={'TESTING'} description={"Unit Tests, SnapShot, Storybook"} titleIcon={faTasks}/>
-                    <Skill title ={'DESIGN'} description={"Material UI, Ant-Design etc"} titleIcon={faSketch}/>
+                    <Skill title={'DEVELOPMENT'} description={"React, Redux, AXIOS etc"} titleIcon={faLaptopCode}/>
+                    <Skill title={'FOUNDATION'} description={"JavaScript, TypeScript, HTML5, CSS3"} titleIcon={faBriefcase}/>
+                    <Skill title={'TESTING'} description={"Unit Tests, SnapShot, Storybook"} titleIcon={faTasks}/>
+                    <Skill title={'DESIGN'} description={"Material UI, Ant-Design etc"} titleIcon={faSketch}/>
                 </div>
-                </div>
+            </div>
         </div>
     );
 }
