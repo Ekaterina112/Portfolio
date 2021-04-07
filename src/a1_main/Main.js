62378482e4aa66e3a83490853e8032499a13ee8b
Ekaterina <ekaterinan112@gmail.com>
2021-04-07 12:08:21 +0300

    deleted button on a main page leave for fixing: 1. make form 2.make modal for 'hire-me-block'/need fix

--- src/a1_main/Main.js
+++ src/a1_main/Main.js
@@ -14,9 +14,9 @@ function Main() {
                     <h5>Hello, my name is</h5>
                     <h1>Ekaterina Naumchik</h1>
                     <h5>Front-end Developer based in #Minsk</h5>
-                    <a href={cv} download={true}>
+                    {/*<a href={cv} download={true}>
                         <Button title={'download my cv'}/>
-                    </a>
+                    </a>*/}
                 </div>
                 <div className={styles.photo}>
                     <img src={photo} alt="it's me"/>
